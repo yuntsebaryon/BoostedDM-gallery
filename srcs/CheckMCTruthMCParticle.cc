0d947c593a90f80056ff42b412299e27a1011734
Yun-Tse Tsai <yuntse@slac.stanford.edu>
2018-08-08 14:59:49 -0700

    add protection from segmentation fault, and correct the momentum-energy conservation calculation

--- srcs/CheckMCTruthMCParticle.cc
+++ srcs/CheckMCTruthMCParticle.cc
@@ -54,16 +54,21 @@ int main( int argc, char ** argv ) {
             
             simb::MCTruth MCTruthObj = MCTruthObjs[iMCTruth];
             int nParticles = MCTruthObj.NParticles();
-            TLorentzVector EventMomentum;
+            TLorentzVector EventMomentum, iP, fP;
             
             // Check the momentum and energy conservation in an interaction
             for ( int iParticle = 0; iParticle < nParticles; ++iParticle ) {
 
                 const simb::MCParticle& MCParticleObj = MCTruthObj.GetParticle( iParticle );
+                std::cout << "GENIE Particle[" << iParticle << "]: PdgCode: " << MCParticleObj.PdgCode() << ", StatudCode: " << MCParticleObj.StatusCode() << std::endl;
                 const TLorentzVector& iMomentum = MCParticleObj.Momentum();
-                EventMomentum += iMomentum;
+                // Initial state particles
+                if ( MCParticleObj.StatusCode() == 0 ) iP += iMomentum;
+                // Stable final state particles and nuclear remnants
+                else if (  MCParticleObj.StatusCode() == 1 || MCParticleObj.StatusCode() == 15 ) fP += iMomentum;
             } // Loop over MCParticles for each MCTruth
             
+            EventMomentum = fP - iP;
             if ( std::abs( EventMomentum.M2() ) > 1e-10 ) {
                 std::cout << "MCTruth " << iMCTruth << " doesn't conserve the momentum and energy.  The total 4-momentum is ( " << EventMomentum.Px() << ", " << EventMomentum.Py() << ", " << EventMomentum.Pz() << ", " << EventMomentum.E() << " )." << std::endl;
             }
@@ -79,13 +84,16 @@ int main( int argc, char ** argv ) {
                 int trackID = thisMCParticle->TrackId();
                 if ( statusCode != 1 ) {
                     std::cout << "MCParticle[" << trackID << "] has the status code " << statusCode << "!" << std::endl;
+                    continue;
                 }
-                const TLorentzVector& Momentum = thisMCParticle->Momentum();
                 
                 // Sanity check: whether the MCParticle propagated to LArG4 is identical to that in GENIE (or other MC generators).
                 // Check PdgCode, StatusCode, and 4-momentum.
                 const sim::GeneratedParticleInfo* thisMCParticleInfo = G4MCParticleInfo[iMCParticle];
                 const simb::MCParticle& thisMCParticleInGenerator = MCTruthObj.GetParticle( thisMCParticleInfo->generatedParticleIndex() );
+                // Check if there is a corresponding GENIE index
+                bool hasGeneratedParticleIndex = thisMCParticleInfo->hasGeneratedParticleIndex();
+                if ( !hasGeneratedParticleIndex ) continue;
                 int refpdgCode = thisMCParticleInGenerator.PdgCode();
                 if ( pdgCode != refpdgCode ) {
                     std::cout << "MCParticle[" << trackID << "] has PDG code difference : GENIE(" << refpdgCode << "), GEANT(" << pdgCode << ")!" << std::endl;
@@ -94,6 +102,16 @@ int main( int argc, char ** argv ) {
                 if ( statusCode != refstatusCode ) {
                     std::cout << "MCParticle[" << trackID << "] has Status code difference : GENIE(" << refstatusCode << "), GEANT(" << statusCode << ")!" << std::endl;
                 }
+                
+
+                if ( thisMCParticle->Process() != "primary" ) {
+                    std::cout << "MCParticle[" << trackID << "] has the process " << thisMCParticle->Process() << "!" << std::endl;
+                    continue;
+                }
+                
+                // std::cout << "MCParticle [" << trackID << "] has index in GENIE: " <<  thisMCParticleInfo->generatedParticleIndex() << std::endl;
+                // std::cout << "Reference MCParticle 4-momentum: (" << thisMCParticleInGenerator.Px() << ", " << thisMCParticleInGenerator.Py() << ", " << thisMCParticleInGenerator.Pz() << ", " << thisMCParticleInGenerator.E() << ")" << std::endl;
+                const TLorentzVector& Momentum = thisMCParticle->Momentum();
                 const TLorentzVector& refMomentum = thisMCParticleInGenerator.Momentum();
                 TLorentzVector diffMomentum = Momentum - refMomentum;
                 if ( std::abs( diffMomentum.M2() ) > 1e-10 ) {
