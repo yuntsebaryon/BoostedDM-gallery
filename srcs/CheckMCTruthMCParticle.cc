298355e5ae4a6d9f7a495e8c4f1dc1b7a4eb34ad
danestocks <dstocks@stanford.edu>
2018-10-04 16:24:55 -0700

    added variable to track # events which do not conserve 4-momentum

--- srcs/CheckMCTruthMCParticle.cc
+++ srcs/CheckMCTruthMCParticle.cc
@@ -38,6 +38,9 @@ int main( int argc, char ** argv ) {
     art::InputTag MCTruthTag { GenLabel };
     art::InputTag MCParticleTag { G4Label };
 
+    // keep track of how many events do not conserve 4-momentum
+    int n_ev = 0;
+
     for ( gallery::Event ev( Filenames ); !ev.atEnd(); ev.next() ) {
 
         std::cout << "Processing "
@@ -46,33 +49,34 @@ int main( int argc, char ** argv ) {
 
         auto const& MCTruthHandle = ev.getValidHandle< std::vector< simb::MCTruth > >( MCTruthTag );
         auto const& MCTruthObjs = *MCTruthHandle;
-        
+
         // Find the MCParticles produced by LArG4 and associated to the MCTruth
         art::FindMany< simb::MCParticle, sim::GeneratedParticleInfo > G4MCParticlesAssn( MCTruthHandle, ev, MCParticleTag );
 
         for ( size_t iMCTruth = 0; iMCTruth < MCTruthObjs.size(); ++iMCTruth ) {
-            
+
             simb::MCTruth MCTruthObj = MCTruthObjs[iMCTruth];
             int nParticles = MCTruthObj.NParticles();
             TLorentzVector EventMomentum, iP, fP;
-            
+
             // Check the momentum and energy conservation in an interaction
             for ( int iParticle = 0; iParticle < nParticles; ++iParticle ) {
 
                 const simb::MCParticle& MCParticleObj = MCTruthObj.GetParticle( iParticle );
-                std::cout << "GENIE Particle[" << iParticle << "]: PdgCode: " << MCParticleObj.PdgCode() << ", StatudCode: " << MCParticleObj.StatusCode() << std::endl;
+                std::cout << "GENIE Particle[" << iParticle << "]: PdgCode: " << MCParticleObj.PdgCode() << ", StatusCode: " << MCParticleObj.StatusCode() << std::endl;
                 const TLorentzVector& iMomentum = MCParticleObj.Momentum();
                 // Initial state particles
                 if ( MCParticleObj.StatusCode() == 0 ) iP += iMomentum;
                 // Stable final state particles and nuclear remnants
                 else if (  MCParticleObj.StatusCode() == 1 || MCParticleObj.StatusCode() == 15 ) fP += iMomentum;
             } // Loop over MCParticles for each MCTruth
-            
+
             EventMomentum = fP - iP;
             if ( std::abs( EventMomentum.M2() ) > 1e-10 ) {
                 std::cout << "MCTruth " << iMCTruth << " doesn't conserve the momentum and energy.  The total 4-momentum is ( " << EventMomentum.Px() << ", " << EventMomentum.Py() << ", " << EventMomentum.Pz() << ", " << EventMomentum.E() << " )." << std::endl;
+                n_ev+=1;
             }
-            
+
             // Find all the (stable final state) MCParticles associated to the MCTruth object
             std::vector< simb::MCParticle const* > const& G4MCParticles = G4MCParticlesAssn.at( iMCTruth );
             std::vector< sim::GeneratedParticleInfo const* > const& G4MCParticleInfo = G4MCParticlesAssn.data( iMCTruth );
@@ -86,7 +90,7 @@ int main( int argc, char ** argv ) {
                     std::cout << "MCParticle[" << trackID << "] has the status code " << statusCode << "!" << std::endl;
                     continue;
                 }
-                
+
                 // Sanity check: whether the MCParticle propagated to LArG4 is identical to that in GENIE (or other MC generators).
                 // Check PdgCode, StatusCode, and 4-momentum.
                 const sim::GeneratedParticleInfo* thisMCParticleInfo = G4MCParticleInfo[iMCParticle];
@@ -102,13 +106,13 @@ int main( int argc, char ** argv ) {
                 if ( statusCode != refstatusCode ) {
                     std::cout << "MCParticle[" << trackID << "] has Status code difference : GENIE(" << refstatusCode << "), GEANT(" << statusCode << ")!" << std::endl;
                 }
-                
+
 
                 if ( thisMCParticle->Process() != "primary" ) {
                     std::cout << "MCParticle[" << trackID << "] has the process " << thisMCParticle->Process() << "!" << std::endl;
                     continue;
                 }
-                
+
                 // std::cout << "MCParticle [" << trackID << "] has index in GENIE: " <<  thisMCParticleInfo->generatedParticleIndex() << std::endl;
                 // std::cout << "Reference MCParticle 4-momentum: (" << thisMCParticleInGenerator.Px() << ", " << thisMCParticleInGenerator.Py() << ", " << thisMCParticleInGenerator.Pz() << ", " << thisMCParticleInGenerator.E() << ")" << std::endl;
                 const TLorentzVector& Momentum = thisMCParticle->Momentum();
@@ -118,9 +122,10 @@ int main( int argc, char ** argv ) {
                     std::cout << "MCParticle[" << trackID << "] has 4-momentum difference: GENIE(" << refMomentum.Px() << ", " << refMomentum.Py() << ", " << refMomentum.Pz() << ", " << refMomentum.E() << "), GEANT(" << Momentum.Px() << ", " << Momentum.Py() << ", " << Momentum.Pz() << ", " << Momentum.E() << ")!" << std::endl;
                 }
                 // End of sanity checks ----------------------------------------------------------------------
-                
+
             } // Loop over MCParticles
         } // Loop over MCTruth
     }
+    std::cout << "Number of events that did not conserve 4-momentum: " << n_ev << std::endl;
     return 0;
 }
